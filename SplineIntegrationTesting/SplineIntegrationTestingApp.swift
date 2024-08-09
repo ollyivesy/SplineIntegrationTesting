61479563e994d8de58a5055eef8366426d8a95ce
Olly Ives <oliver.ives@integrelsolutions.com>
2024-08-09 15:44:39 +0100

    Initial Commit

---
+++ SplineIntegrationTesting/SplineIntegrationTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplineIntegrationTestingApp.swift
+//  SplineIntegrationTesting
+//
+//  Created by Olly Ives on 09/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SplineIntegrationTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
